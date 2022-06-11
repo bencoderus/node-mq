24b512a5b455243df91bc2344307029c60575501
Benjamin Iduwe <bencoderus@gmail.com>
2022-06-11 22:19:18 +0100

    Use secret instead of config

--- src/consumer.ts
+++ src/consumer.ts
@@ -7,4 +7,5 @@ const queue = MessageQueue.getQueue(channels.NOTIFICATION_QUEUE);
 queue.consume((message: ConsumeMessage) => {
   console.log(message.content.toString());
   queue.ack(message);
+  console.log("Message processed successfully.");
 });
